48057c5c656009260430e27f7f5d7455110893ad
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-04-25 20:46:51 +0900

    test

---
+++ skill/get-key.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const cache = require("memory-cache");
+
+module.exports = class SkillSaveKey {
+    constructor(){
+        this.required_parameter = {
+            service: {
+                message_to_confirm: {
+                    type: "text",
+                    text: "アプリ名またはサービス名を教えてください。"
+                }
+            }
+        }
+    }
+
+    finish(bot, event, context, resolve, reject) => {
+        let keychain = cache.get(`keychain_${bot.extract_sender_id()}`);
+
+        let key = keychain.find(key => key.service === context.confirmed.service);
+
+        let message;
+        if (key){
+            message = {
+                type: "text",
+                text: `ユーザー名: ${key.username}\nパスワード: ${key.password}`
+            }
+        } else {
+            message = {
+                type: "text",
+                text: `該当の情報はありませんでした。`
+            }
+        }
+        return bot.reply(message).then((response) => {
+            return resolve(;)
+        })
+    }
+}
---
+++ skill/save-key.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const cache = require("memory-cache");
+
+module.exports = class SkillSaveKey {
+    constructor(){
+        this.required_parameter = {
+            service: {
+                message_to_confirm: {
+                    type: "text",
+                    text: "アプリ名またはサービス名を教えてください。"
+                }
+            },
+            username: {
+                message_to_confirm: {
+                    type: "text",
+                    text: "ユーザー名を教えてください。"
+                }
+            },
+            password: {
+                message_to_confirm: {
+                    type: "text",
+                    text: "パスワードを教えてください。"
+                }
+            }
+        }
+    }
+
+    finish(bot, event, context, resolve, reject) => {
+        let keychain = cache.get(`keychain_${bot.extract_sender_id()}`);
+        if (!keychain) keychain = [];
+
+        keychain.push({
+            service: context.confirmed.service,
+            username: context.confirmed.username,
+            password: context.confirmed.password
+        });
+        cache.put(`keychain_${bot.extract_sender_id()}`, keychain)
+
+        return bot.reply({
+            type: "text",
+            text: `${context.confirmed.service}のユーザー名・パスワードを保存しました。`
+        }).then((response) => {
+            return resolve();
+        })
+    }
+}
